67ef0148d619bad0d32f0d6471b496dc1a5dcfcf
entezarh173 <entezar.h173@gmail.com>
2023-02-11 00:51:55 -0800

    create slider session

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,35 +1,52 @@
-let searchTopBox = document.querySelector('.search-top-box');
-let searchBox = document.querySelector('.search-box');
-let searchInputTop = document.querySelector('.search-input-top');
-let cover = document.querySelector('.cover');
-let categoryMenu = document.querySelector('.category-menu'); 
-let categoryMenuList = document.querySelector('.category-menu-list'); 
+let searchTopBox = document.querySelector(".search-top-box");
+let searchBox = document.querySelector(".search-box");
+let searchInputTop = document.querySelector(".search-input-top");
+let cover = document.querySelector(".cover");
+let categoryMenu = document.querySelector(".category-menu");
+let categoryMenuList = document.querySelector(".category-menu-list");
 
-
-searchTopBox.addEventListener('click',(event)=>{
-    searchBox.classList.add('active');
-    cover.classList.add('active');
-
+searchTopBox.addEventListener("click", (event) => {
+  searchBox.classList.add("active");
+  cover.classList.add("active");
 });
-cover.addEventListener('click',(event)=>{
-    searchBox.classList.remove('active');
-    cover.classList.remove('active');
+cover.addEventListener("click", (event) => {
+  searchBox.classList.remove("active");
+  cover.classList.remove("active");
 });
 
-categoryMenu.addEventListener('mousemove',()=>{
-    categoryMenuList.style.display = 'block';
-    document.body.style.overflow = 'hidden';
+categoryMenu.addEventListener("mousemove", () => {
+  categoryMenuList.style.display = "block";
+  document.body.style.overflow = "hidden";
 });
 // categoryMenuList.addEventListener('mousemove',()=>{
 //     categoryMenuList.style.display = 'block';
 //     document.body.style.overflow = 'hidden';
 // });
 
-categoryMenu.addEventListener('mouseleave',()=>{
-    categoryMenuList.style.display = 'none';
-    document.body.style.overflow = 'auto';
+categoryMenu.addEventListener("mouseleave", () => {
+  categoryMenuList.style.display = "none";
+  document.body.style.overflow = "auto";
 });
 // categoryMenu.addEventListener('mouseleave',()=>{
 //     categoryMenuList.style.display = 'none';
 //     document.body.style.overflow = 'auto';
 // });
+
+//slider
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    nav: true,
+    navText: "<>",
+    autoplay: true,
+    autoplayTimeout: 7000,
+  });
+});
+let elem = document.querySelector(".slider");
+elem.addEventListener("mousemove", () => {
+  document.querySelector(".owl-nav").classList.add("active");
+});
+elem.addEventListener("mouseleave", () => {
+  document.querySelector(".owl-nav").classList.remove("active");
+});
